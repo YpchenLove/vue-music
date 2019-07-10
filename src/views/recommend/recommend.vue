355e78ff4447f355d21c83d47c051263a49c9392
ypchen <372649620@qq.com>
2019-07-10 20:49:08 +0800

    feat: 新增接口配置和首页

--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -1,13 +1,130 @@
 <template>
   <div class="recommend">
-    recommend
+    <div class="recommend-content">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommends" :key="item.id">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" />
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌曲推荐</h1>
+          <ul>
+            <li class="item" v-for="item in discList" :key="item.dissid">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl" />
+              </div>
+              <div class="text">
+                <h2 class="name" v-text="item.creator.name"></h2>
+                <h2 class="desc" v-text="item.dissname"></h2>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Slider from '../../base/slider/slider'
+
+import { getRecommend, getDiscList } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+
+export default {
+  data () {
+    return {
+      recommends: [],
+      discList: []
+    }
+  },
+  components: {
+    Slider
+  },
+  created () {
+    this._getRecommend()
+    this._getDiscList()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          const slider = res.data.slider
+          this.recommends = slider
+        }
+      })
+    },
+    _getDiscList () {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          const discList = res.data.list
+          this.discList = discList
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
-
+@import '~common/stylus/variable'
+.recommend
+  position: fixed
+  width: 100%
+  top: 88px
+  bottom: 0
+  .recommend-content
+    // height: 100%
+    // overflow: hidden
+    // .slider-wrapper
+    // position: relative
+    // width: 100%
+    // height: 0
+    // padding-top: 40%
+    // overflow: hidden
+    // .slider-content
+    // position: absolute
+    // top: 0
+    // left: 0
+    // width: 100%
+    // height: 100%
+    .recommend-list
+      .list-title
+        height: 65px
+        line-height: 65px
+        text-align: center
+        font-size: $font-size-medium
+        color: $color-theme
+      .item
+        display: flex
+        box-sizing: border-box
+        align-items: center
+        padding: 0 20px 20px 20px
+        .icon
+          flex: 0 0 60px
+          width: 60px
+          padding-right: 20px
+        .text
+          display: flex
+          flex-direction: column
+          justify-content: center
+          flex: 1
+          line-height: 20px
+          overflow: hidden
+          font-size: $font-size-medium
+          .name
+            margin-bottom: 10px
+            color: $color-text
+          .desc
+            color: $color-text-d
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>

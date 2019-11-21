c02c9c70854db255198012b5276dc3496e9f06d6
Ypchen <ypchng@qq.com>
2019-11-21 14:46:33 +0800

    feat: 歌手数据处理

--- src/views/singer/singer.vue
+++ src/views/singer/singer.vue
@@ -59,7 +59,23 @@ export default {
         )
       })
 
-      console.log(map)
+      let hot = []
+      let ret = []
+      for (const key in map) {
+        if (map.hasOwnProperty(key)) {
+          const val = map[key]
+
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
   }
 }
